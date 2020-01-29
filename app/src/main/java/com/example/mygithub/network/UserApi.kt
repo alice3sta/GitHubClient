a526ed8a220025c7a27c9abb65a018a1a2cb5a36
alisa <a.khukhriankina@gmail.com>
2020-01-29 20:04:23 +0300

    Добавлен запрос на полные данные пользователя

--- app/src/main/java/com/example/mygithub/network/UserApi.kt
+++ app/src/main/java/com/example/mygithub/network/UserApi.kt
@@ -1,8 +1,10 @@
 package com.example.mygithub.network
 
-import com.example.mygithub.ResponseListUser
+import com.example.mygithub.objects.ResponseListUser
+import com.example.mygithub.objects.UserFullInfo
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UserApi {
@@ -10,4 +12,9 @@ interface UserApi {
     fun getUserInfo(
         @Query("q") userName: String
     ): Observable<ResponseListUser>
+
+    @GET("/users/{username}")
+    fun getOneUser(
+        @Path("username") userName: String
+    ): Observable<UserFullInfo>
 }
