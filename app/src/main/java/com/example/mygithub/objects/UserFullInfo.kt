18e3f2f5d76d73a1ea1619edb10756b21488c5c1
alisa <a.khukhriankina@gmail.com>
2020-01-29 20:07:52 +0300

    Добавлен класс UserFullInfo

---
+++ app/src/main/java/com/example/mygithub/objects/UserFullInfo.kt
@@ -0,0 +1,13 @@
+package com.example.mygithub.objects
+
+import com.google.gson.annotations.SerializedName
+
+class UserFullInfo(
+    @SerializedName("login") val userLogin: String,
+    @SerializedName("id") val userId: Long,
+    @SerializedName("avatar_url") val userAvatar: String,
+    @SerializedName("name") val userName: String,
+    @SerializedName("public_repos") val publicRepos: Int,
+    @SerializedName("location") val location: String,
+    @SerializedName("created_at") val createdAt: String = ""
+)
